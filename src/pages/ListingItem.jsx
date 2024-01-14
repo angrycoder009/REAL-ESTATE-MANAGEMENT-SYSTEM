360661f49f8892daa6407e5e62f817f7a2535c83
gopal yadav <yadavgopal6675@gmail.com>
2024-01-15 02:31:33 +0530

    listing card updated with the values

--- src/pages/ListingItem.jsx
+++ src/pages/ListingItem.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
+import { MdLocationPin } from "react-icons/md";
 export default function ListingItem({listing,id}) {
-  return (
-    <div>
-      {listing.name}
-    </div>
-  )
+  return   <li className='mt-6 mr-10'>
+    <Link to={`/category/${listing.type}/${id}`}>
+      <img className='rounded-xl w-80 h-[300px] object-cover ' src={listing.imgUrls[0]} alt="" />
+      <Moment fromNow>{listing.timestamp?.toDate()}</Moment>
+      <div>
+        <div>
+        <MdLocationPin />
+        <p>{listing.address}</p>
+        </div>  
+       <p>{listing.name}</p>
+       <p>${listing.offer? listing.discountedPrice .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",") :listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+           {listing.type === "rent" && "/month"}       
+                  </p>
+      </div>
+    </Link>
+  </li>;
+  
 }
